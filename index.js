c0d4c9360c5b34284a74bceb9d807056beec7ce3
Maksym Bykovskyy <maksym@bykovskyy.com>
2014-08-19 22:02:35 -0700

    Refactored ref collection and watching to use promises. Fixed watching deeper links.

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ var vulcanize = require('vulcanize');
 var path = require('path');
 var mkdirp = require('mkdirp');
 var clone = require('clone');
-var fs = require('fs');
-var cheerio = require('cheerio');
-var mapSeries = require('promise-map-series');
+var RefWatcher = require('./lib/ref-watcher');
+var RefScraper = require('./lib/ref-scraper');
+var RefWalker = require('./lib/ref-walker');
 
 module.exports = Vulcanize;
 Vulcanize.prototype = Object.create(Writer.prototype);
@@ -25,59 +25,34 @@ function Vulcanize(inputTree, options) {
   this.outputFilepath = options.output || path.basename(options.input);
 }
 
-function exclude(excludes, ref) {
-  return excludes && excludes.some(function(exclude) {
-    return exclude.test(ref);
+Vulcanize.prototype.vulcanize = function(options) {
+  return new RSVP.Promise(function(resolve, reject) {
+    vulcanize.setOptions(options, function(error) {
+      if (error) {
+        reject(error);
+      }
+
+      mkdirp(path.dirname(options.output), function(error) {
+        if (error) {
+          reject(error);
+        }
+
+        vulcanize.processDocument();
+        resolve();
+      });
+    });
   });
-}
+};
 
-function processRef(ref, excludes, outputDir, watchRefs) {
-  if (!exclude(excludes, ref)) {
-    var refPath = path.resolve(outputDir, ref);
+Vulcanize.prototype.watchRefs = function(readTree, file, excludes) {
+  var watcher = new RefWatcher(readTree);
+  var scraper = new RefScraper(excludes);
+  var walker = new RefWalker(scraper, watcher);
 
-    if (fs.existsSync(refPath)) {
-      watchRefs.push(path.dirname(refPath));
-    }
-  }
-}
-
-function processStyles($, excludes, outputDir, watchRefs) {
-  $('link[rel="stylesheet"][href]').each(function() {
-    processRef($(this).attr('href'), excludes, outputDir, watchRefs);
+  return walker.traverse(file).then(function() {
+    return watcher.watch();
   });
-}
-
-function processScripts($, excludes, outputDir, watchRefs) {
-  $('script[src]').each(function() {
-    processRef($(this).attr('src'), excludes, outputDir, watchRefs);
-  });
-}
-
-function processImports($, excludes, outputDir, watchRefs) {
-  $('link[rel="import"][href]').each(function() {
-    processRef($(this).attr('href'), excludes, outputDir, watchRefs);
-  });
-}
-
-
-function collectRefs(file, excludes) {
-  excludes = excludes || {};
-  var content = fs.readFileSync(file, 'utf8');
-  var $ = cheerio.load(content);
-  var outputDir = path.dirname(file);
-  var watchRefs = [];
-
-  processStyles($, excludes.styles, outputDir, watchRefs);
-  processScripts($, excludes.scripts, outputDir, watchRefs);
-  processImports($, excludes.imports, outputDir, watchRefs);
-
-  return watchRefs;
-}
-
-function watchRefs(readTree, file, excludes) {
-  var refs = collectRefs(file, excludes);
-  return mapSeries(refs, readTree);
-}
+};
 
 Vulcanize.prototype.write = function(readTree, destDir) {
   // We have to clone options again as vulcanize changes the hash which causes
@@ -88,23 +63,8 @@ Vulcanize.prototype.write = function(readTree, destDir) {
   return readTree(this.inputTree).then(function(srcPath) {
     options.input = path.join(srcPath, options.input);
 
-    return new RSVP.Promise(function(resolve, reject) {
-      vulcanize.setOptions(options, function(error) {
-        if (error) {
-          reject(error);
-        }
-
-        mkdirp(path.dirname(options.output), function(error) {
-          if (error) {
-            reject(error);
-          }
-
-          vulcanize.processDocument();
-          resolve();
-        });
-      });
-    }).then(function() {
-      return watchRefs(readTree, options.input, options.excludes);
-    });
-  });
+    return this.vulcanize(options).then(function() {
+      return this.watchRefs(readTree, options.input, options.excludes);
+    }.bind(this));
+  }.bind(this));
 };
