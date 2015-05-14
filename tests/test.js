2884a02a63c50edb07a920757174dd441048db7f
Maksym Bykovskyy <maksym@bykovskyy.com>
2015-05-13 22:03:52 -0700

    Bumped vulcanize version and added cyclic dependency test which is failing.

--- tests/test.js
+++ tests/test.js
@@ -8,37 +8,49 @@ var builder;
 
 afterEach(function() {
   if (builder) {
-     builder.cleanup();
+    builder.cleanup();
   }
 });
 
 it('should vulcanize components', function() {
   var tree = vulcanize('fixtures', {
-    input: 'index.html'
+    input: 'basic-index.html'
   });
   builder = new Broccoli.Builder(tree);
 
   return builder.build().then(function(result) {
-    var indexHtml = path.join(result.directory, 'index.html');
+    var indexHtml = path.join(result.directory, 'basic-index.html');
+    assert(fs.existsSync(indexHtml));
+  });
+});
+
+it('should vulcanize cyclic dependent components', function() {
+  var tree = vulcanize('fixtures', {
+    input: 'cyclic-dependency-index.html'
+  });
+  builder = new Broccoli.Builder(tree);
+
+  return builder.build().then(function(result) {
+    var indexHtml = path.join(result.directory, 'cyclic-dependency-index.html');
     assert(fs.existsSync(indexHtml));
   });
 });
 
 it('should not change options', function() {
   var options = {
-    input: 'index.html'
+    input: 'basic-index.html'
   };
   var tree = vulcanize('fixtures', options);
   builder = new Broccoli.Builder(tree);
 
   return builder.build().then(function() {
-    assert.deepEqual({input: 'index.html'}, options);
+    assert.deepEqual({input: 'basic-index.html'}, options);
   });
 });
 
 it('should not be affected by options changing outside', function() {
   var options = {
-    input: 'index.html'
+    input: 'basic-index.html'
   };
   var tree = vulcanize('fixtures', options);
   options.output = 'should/not/affect/vulcanize.html';
@@ -46,7 +58,7 @@ it('should not be affected by options changing outside', function() {
   builder = new Broccoli.Builder(tree);
 
   return builder.build().then(function(result) {
-    var indexHtml = path.join(result.directory, 'index.html');
+    var indexHtml = path.join(result.directory, 'basic-index.html');
     assert(fs.existsSync(indexHtml));
   });
 });
@@ -54,7 +66,7 @@ it('should not be affected by options changing outside', function() {
 it('should rename vulcanized component', function() {
   var outputFilePath = 'vulcanized/vulcanized.html';
   var tree = vulcanize('fixtures', {
-    input: 'index.html',
+    input: 'basic-index.html',
     output: outputFilePath
   });
   builder = new Broccoli.Builder(tree);
@@ -67,16 +79,16 @@ it('should rename vulcanized component', function() {
 
 it('should be able to call vulcanize repeatedly', function() {
   var tree = vulcanize('fixtures', {
-    input: 'index.html'
+    input: 'basic-index.html'
   });
   builder = new Broccoli.Builder(tree);
 
   return builder.build().then(function(result) {
-    var indexHtml = path.join(result.directory, 'index.html');
+    var indexHtml = path.join(result.directory, 'basic-index.html');
     assert(fs.existsSync(indexHtml));
 
     return builder.build().then(function(result) {
-      var indexHtml = path.join(result.directory, 'index.html');
+      var indexHtml = path.join(result.directory, 'basic-index.html');
       assert(fs.existsSync(indexHtml));
     });
   });
@@ -92,12 +104,12 @@ it('should accept a broccoli tree', function() {
   };
 
   tree = vulcanize(tree, {
-    input: 'index.html'
+    input: 'basic-index.html'
   });
   builder = new Broccoli.Builder(tree);
 
   return builder.build().then(function(result) {
-    var indexHtml = path.join(result.directory, 'index.html');
+    var indexHtml = path.join(result.directory, 'basic-index.html');
     assert(fs.existsSync(indexHtml));
   });
 });
